cd651ea5f181b3b4e12e9f2f790b882ad5ad1e36
AkshitaDanewala <akshitadanewala@gmail.com>
2024-01-27 13:31:43 +0530

    create nav section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Nav from "./Components/Navbar"
+import Footer from "./Components/Footer"
+import Home from "./Pages/Home"
+import { Route, Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+<Nav/>
+
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+
+
+</Routes>
+
+
+
+<Footer/>
+
+
+    </div>
   )
 }
 
